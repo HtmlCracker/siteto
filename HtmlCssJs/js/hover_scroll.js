c07e04c4846ffb0e0090935a1ecb912cbb844bdb
HtmlCracker <daniilprogrammer3.14@gmail.com>
2023-07-14 14:44:53 +0300

    поменял js

--- HtmlCssJs/js/hover_scroll.js
+++ HtmlCssJs/js/hover_scroll.js
@@ -4,7 +4,6 @@ function sleep(ms) {
 
 $(async function() {
     var line = $('.under-line');
-    await sleep(500);
 
     var links = {
         'about_link': '#about_us-header',
@@ -15,7 +14,7 @@ $(async function() {
     $(".header-link").hover(
         function () {
             $(this).css('color', 'rgb(222, 222, 222)');
-            line.animate({'left': $(this).offset().left, 'width': $(this).width()}, 250);
+            line.animate({'left': $(this).offset().left, 'width': $(this).width()}, 350);
         }, function () {
             $(this).css('color', 'white');
         }
