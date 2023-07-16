b46fdb026448449b2e0f0bf0138c08e80771ae39
HtmlCracker <daniilprogrammer3.14@gmail.com>
2023-07-16 17:07:10 +0300

    убрал кринж из main.js

--- HtmlCssJs/js/main.js
+++ HtmlCssJs/js/main.js
@@ -1,25 +1,22 @@
-var word_list = ['быстро', 'красиво', 'безопасно'];
-var sighn_list = ['-', '=', '#', '@', '!', '/', '$',
+const word_list = ['быстро', 'красиво', 'безопасно'];
+const sighn_list = ['-', '=', '#', '@', '!', '/', '$',
                  '%', '(', '{', '}', '&', '<', 'Ђ',
                  '*', '^', 'ɣ']
 
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+const sleep = (ms) => { return new Promise(resolve => setTimeout(resolve, ms)); }
 
-function choose() {
+const choose = () => {
     var index = Math.floor(Math.random() * sighn_list.length);
     return sighn_list[index];
 }
 
-function randomInteger(min, max) {
+const randomInteger = (min, max) => {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
 
-function findLongestWord(lst)
-{
+const findLongestWord = (lst) => {
     var longestWord = 0;
     for(var i = 0; i < lst.length; i++)
     {
@@ -33,22 +30,19 @@ function findLongestWord(lst)
 
 let count = 0;
 
-function add_div(letter)
-{   
+const add_div = (letter) => {   
     count++;
 
     let div = `<div class="letters" id="${count-1}">${letter}</div>`;
     $('.all_letters').append(div);
 };
 
-function rand_sighn(word)
-{   
-    for (let i=0; i<word.length; i++)
-    {   
-        if (word[i] != answer_list[i])
-        {
-            if (randomInteger(1,15) == 5)
-            {   
+const rand_sighn = (word) => {   
+    for (let i=0; i<word.length; i++) {
+
+        if (word[i] != answer_list[i]) {
+
+            if (randomInteger(1,15) == 5) {   
                 answer_list[i] = word[i];
             } else {
                 answer_list[i] = choose();
@@ -58,25 +52,21 @@ function rand_sighn(word)
     return answer_list;
 };
 
-function new_div(word, trying)
-{   
-    for (let i=0; i < findLongestWord(word_list); i++)
-    {   
+const new_div = (word) => {
+    for (let i=0; i < findLongestWord(word_list); i++) {
+        const el_jq = $(`#${i}`);
+
         if (i < word.length) {
-            $(`#${i}`).html(`<div class="letters" id="${i}">${word[i]}</div>`);
-            $(`#${i}`).css('display', 'inline-block');
-        }
-        else {
-            $(`#${i}`).css('display', 'none');
+            el_jq.html(`<div class="letters" id="${i}">${word[i]}</div>`);
+            el_jq.css('display', 'inline-block');
+        } else {
+            el_jq.css('display', 'none');  
         }
     };
 };
 
-$(async function()
-{   
-
-    for (let j=0; j < findLongestWord(word_list); j++)
-    {   
+$(async () => {   
+    for (let j=0; j < findLongestWord(word_list); j++) {   
         add_div('-');
     }
     
@@ -84,22 +74,18 @@ $(async function()
 
     while (true) {
 
-    for (let word_ind=0; word_ind < word_list.length; word_ind++)
-    {      
+    for (let word_ind=0; word_ind < word_list.length; word_ind++) {      
             answer_list = [];
 
-            for (let i=0; i < word_list[word_ind].length; i++)
-            {
+            for (let i=0; i < word_list[word_ind].length; i++) {
                 answer_list.push(' ');
             };
-            for (let trying=0; trying < 15; trying++)
-            {   
-                if (answer_list == word_list[word_ind].split(''))
-                {
+            for (let trying=0; trying < 15; trying++) {   
+                if (answer_list == word_list[word_ind].split('')) {
                     break;
                 } else {
                     await sleep(90);
-                    new_div(rand_sighn(word_list[word_ind]), trying);
+                    new_div(rand_sighn(word_list[word_ind]));
                 }
             };
             answer_list = word_list[word_ind].split('');
