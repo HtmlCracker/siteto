cbf182efa45abf5dedfca9fa82aa44aa2fecc2a2
HtmlCracker <daniilprogrammer3.14@gmail.com>
2023-08-11 20:50:11 +0300

    раскидал код с hover и scroll по разным файлам

--- HtmlCssJs/js/scroll.js
+++ HtmlCssJs/js/scroll.js
@@ -73,40 +73,7 @@ animation_lines = async () => {
 
 }
 
-$(async () => {
-    var line = $('.under-line');
-    var is_line_left = true;
 
-    var links = {
-        'about_link': '#about_us-header',
-        'portf_link': '.portf-h1-div',
-        'cont_link': 'footer'
-    };
-
-    $(".header-link").hover(
-        function () {
-            $(this).css('color', 'rgb(222, 222, 222)');
-            line.animate({'left': $(this).offset().left, 'width': $(this).width()}, 350);
-            is_line_left = false;
-        }, function () {
-            $(this).css('color', 'white');
-        }
-    );
-
-    $('.img-about, body').hover(
-        function () {
-            if (!(is_line_left)) {
-                line.animate({'left': '-200px'}, 350);
-                is_line_left = true;
-            }
-        }
-    );
-
-    $('.header-link').on('click', function () {
-        $('html, body').animate({ scrollTop: $(links[this.id]).offset().top }, 350);
-    });
-
-});
 
 var count_scroll_second = 0;
 var count_portf = 0;
